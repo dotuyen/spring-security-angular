1eea22ed2570caf29ac1e98528b880cf7fd65838
tuyendt <tuyendt@itsol.vn>
2021-08-16 19:48:12 +0700

    init project

---
+++ src/test/java/com/tuyen/springsecurityangular/SpringSecurityAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyen.springsecurityangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
