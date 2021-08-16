c91733390c7c8fa45e4b6bbc22ab39dded7afafd
tuyendt <tuyendt@itsol.vn>
2021-08-16 20:13:01 +0700

    add basic authen logic code.

---
+++ src/main/java/com/tuyen/springsecurityangular/bean/AuthenticationBean.java
@@ -0,0 +1,12 @@
+package com.tuyen.springsecurityangular.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationBean {
+    private String message;
+}
